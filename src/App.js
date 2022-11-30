6307a80420a6844c04aed65e1e8feb5d9761a926
mac-ricoland <ricoland@macbook-pro.lan>
2022-11-30 15:22:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+
+// import
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faListAlt } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+library.add(faTrash, faListAlt);
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTab = [...tasks];
+    newTab.push({
+      title: input,
+      toDo: false,
+    });
+    setTasks(newTab);
+    setInput("");
+  };
+
+  const handleCkeck = (index) => {
+    const newTab = [...tasks];
+    newTab[index].toDo = !newTab[index].toDo;
+    setTasks(newTab);
+  };
+
+  const handleDelete = (index) => {
+    const newTab = [...tasks];
+    newTab.splice(index, 1);
+    setTasks(newTab);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <div className="list">
+          <div className="all-tasks">
+            {tasks.map((task, index) => {
+              return (
+                <div className="task" key={index}>
+                  <input
+                    checked={task.toDo ? true : false}
+                    type="checkbox"
+                    onChange={() => {
+                      handleCkeck(index);
+                    }}
+                  />
+                  <span className={task.toDo ? "isDone" : null}>
+                    {task.title}
+                  </span>
+                  <span>
+                    <FontAwesomeIcon
+                      className="icon-trash"
+                      icon="trash"
+                      onClick={() => {
+                        handleDelete(index);
+                      }}
+                    />
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <div className="input-btn">
+              <input
+                className="input"
+                type="text"
+                placeholder="New task..."
+                value={input}
+                onChange={(event) => {
+                  setInput(event.target.value);
+                }}
+              />
+              <input className="btnAddTask" type="submit" value={"Add task"} />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Header = () => {
+  return (
+    <div className="container-header">
+      <FontAwesomeIcon icon="rectangle-list" />
+      <span>ToDo List</span>
+    </div>
+  );
+};
+
+export default Header;
